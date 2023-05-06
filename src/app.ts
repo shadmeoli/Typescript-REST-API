ce98c5edc16aa7ad7f26c71885bc005a0f7233b8
shadmeoli <shadcodes@gmail.com>
2023-05-06 15:19:49 +0300

    INFO: API update

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Server Running')
+import express from "express";
+import path from "path";
+require("dotenv").config();
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use("/v1/api/index", indexRouter);
+app.use("/v1/api/index/users", usersRouter);
+
+module.exports = app;
